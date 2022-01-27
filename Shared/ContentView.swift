7852f6f63e71499cb8706b17a78e35b3344a9328
Raoul Koopman <rkoop88@gmail.com>
2022-01-26 19:01:02 -0500

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by R K on 1/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("First Repo!")
+            .background(.green)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/FirstRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstRepoApp.swift
+//  Shared
+//
+//  Created by R K on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
